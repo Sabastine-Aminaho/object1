baf7e637fb7298a49201486961d4d8b11e3ca202
Ndukaegho Aminaho <sabastineaminaho3@gmail.com>
2022-06-11 21:28:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var myCar = {
+"my color": "Red",
+   "My Tires": "2 pairs",
+    Glass: "yes",
+    Name: ["G Wagon"],
+};
+myCar.wheel = "Star";
+myCar["dog"] = "wah"
+delete myCar["my color"];
+myCar;
